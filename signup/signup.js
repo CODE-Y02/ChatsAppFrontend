996e1003c91406afb5c001945c9ea8f6c78ef7ae
yatharth <lakhatey@gmail.com>
2022-11-25 19:06:03 +0530

    added api to signup frontend

---
+++ signup/signup.js
@@ -0,0 +1,40 @@
+document.getElementById("authForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    password: document.getElementById("password").value,
+  };
+
+  signUpUser(user);
+});
+
+async function signUpUser(userObj) {
+  try {
+    let response = await axios.post("http://localhost:3000/signup", userObj);
+
+    // console.log(response);
+
+    alert("signup successfull 😎 ");
+
+    window.location = "/login/login.html";
+  } catch (error) {
+    console.log("err ===> \n", error.response.data);
+
+    showErrMsg(error.response.data.message, 3000);
+  }
+}
+
+function showErrMsg(msg, timeout) {
+  let errorMsgElement = document.getElementById("error-message");
+  errorMsgElement.classList.remove("hidden");
+
+  errorMsgElement.innerText = msg;
+
+  setTimeout(() => {
+    errorMsgElement.innerText = "";
+    errorMsgElement.classList.add("hidden");
+  }, timeout);
+}
